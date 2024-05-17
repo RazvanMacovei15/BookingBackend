7025f62b491c0bdfa3965a47ee9342433a954568
RazvanMacovei15 <razvanmc15@gmail.com>
2024-05-17 16:35:53 +0300

    commit de la lucru

--- src/main/java/siemens/booking/service/HotelService.java
+++ src/main/java/siemens/booking/service/HotelService.java
@@ -2,7 +2,6 @@ package siemens.booking.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import siemens.booking.dto.requests.SaveHotelRequest;
 import siemens.booking.entity.Hotel;
 import siemens.booking.repository.HotelRepository;
 
@@ -11,15 +10,4 @@ import siemens.booking.repository.HotelRepository;
 public class HotelService {
     private final HotelRepository hotelRepository;
 
-    public long save(SaveHotelRequest request) {
-        Hotel hotel = Hotel.builder()
-                .id(request.getId())
-                .name(request.getName())
-                .longitude(request.getLongitude())
-                .latitude(request.getLatitude())
-                .build();
-
-        Hotel savedHotel = hotelRepository.save(hotel);
-        return savedHotel.getId();
-    }
 }
